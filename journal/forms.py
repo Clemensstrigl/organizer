bf097b04b0bb8eb020f9c0e6ca8766f135afd380
cstrigl <cstrigl@mail.csuchico.edu>
2021-10-23 17:39:13 -0700

    organizer started

---
+++ journal/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from journal.models import JournalEntry
+
+class JournalEntryForm(forms.ModelForm):
+	description = forms.CharField(widget=forms.TextInput(attrs={'size': '80'}))
+	entry = forms.CharField(widget=forms.Textarea(
+		attrs={'rows': '8', 'cols': '80'}))
+
+	class Meta():
+		model = JournalEntry
+		fields = ('description', 'entry')
---
+++ journal/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class JournalEntry(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	datetime = models.DateField(auto_now=True)
+	description = models.CharField(max_length=128)
+	entry = models.CharField(max_length=65536)
