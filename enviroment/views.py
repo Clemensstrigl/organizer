44417bb898d07ea119b1cefe0adb0b54b9589db2
cstrigl <cstrigl@mail.csuchico.edu>
2021-10-24 01:48:23 -0700

    adding models

---
+++ enviroment/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from app1.forms import ChessForm, JoinForm, LoginForm
+from journal.models import JournalEntry
+from tasks.models import TaskEntry 
+
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'app1/home.html', page_data)
+
+def about(request):
+    return render(request,'app1/about.html')
+
+def join(request):
+    if (request.method == "POST"):
+        join_form = JoinForm(request.POST)
+        if (join_form.is_valid()):
+            user = join_form.save()
+            user.set_password(user.password)
+            user.save()
+            return redirect("/")
+        else:
+            page_data = { "join_form": join_form }
+            return render(request, 'app1/join.html', page_data)
+    else:
+        join_form = JoinForm()
+        page_data = { "join_form": join_form }
+        return render(request, 'app1/join.html', page_data)
+
+def user_login(request):
+    if (request.method == 'POST'):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data["username"]
+            password = login_form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request,user)
+                    return redirect("/")
+                else:
+                    return HttpResponse("Your account is not active.")
+            else:
+                print("Someone tried to login and failed.")
+                print("They used username: {} and password: {}".format(username,password))
+                return render(request, 'app1/login.html', {"login_form": LoginForm})
+    else:
+        return render(request, 'app1/login.html', {"login_form": LoginForm})
+
+@login_required(login_url='/login/')
+def user_logout(request):
+    logout(request)
+    return redirect("/")
